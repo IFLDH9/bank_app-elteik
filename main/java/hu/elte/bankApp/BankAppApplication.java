fdbd809675732566ae1084cb3a6d377a3c060c63
IFLDH9 <43792573+IFLDH9@users.noreply.github.com>
2019-10-28 17:32:42 +0100

    spring init

---
+++ main/java/hu/elte/bankApp/BankAppApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.bankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppApplication.class, args);
+	}
+
+}
