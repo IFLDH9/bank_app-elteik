f7733ba9bdc4f3a85080802c3b6787ea1d1ca2c0
IFLDH9 <43792573+IFLDH9@users.noreply.github.com>
2019-10-28 17:33:58 +0100

    spring init

---
+++ src/test/java/hu/elte/bankApp/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.elte.bankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
