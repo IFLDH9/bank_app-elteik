710e801d08139d0a15ef81f3b2a9ff9345139dd9
IFLDH9 <43792573+IFLDH9@users.noreply.github.com>
2019-12-10 15:30:12 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,2 @@
-INSERT INTO account ( created_at, account_number, balance) VALUES ( CURRENT_TIMESTAMP(), '33334444 55556666', 999999);
-INSERT INTO account ( created_at, account_number, balance) VALUES ( CURRENT_TIMESTAMP(), '11112222 333344444', 1);
-INSERT INTO card (card_number, card_type, expiration_date, owner_name, account_id) VALUES ('1234 5678 9101 1121', 'MASTERCARD', '2020-03-02', 'elso tulajdonos', 1);
 INSERT INTO personal_data(name,password, date_of_birth, id_card_number,role) VALUES ('admin','$2y$10$XNEqE1kjReYfX8bFhkFnRuxxqIb5WnUmcDMZFvpeplssGW6I9TgJm' ,'1991-01-01', 'MM12345','ROLE_ADMIN');
-INSERT INTO account_owners(accounts_id, owners_id) VALUES (1,1);
-INSERT INTO transaction (date_of_transaction, source_account_number, target_account_number, value, source_account_id, target_account_id)
-							VALUES (current_timestamp(), '33334444 55556666', '11112222 333344444', 2, 2, 1);
+
